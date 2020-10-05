b7d7bc65565ec3ff25361453ec53e84aa79acd00
Sushmita Ray <48674421+Sushmitaray@users.noreply.github.com>
2020-10-05 11:45:07 -0700

    Delete comments.py

--- comments.py
+++
@@ -1,7 +0,0 @@
-# comments
-''' this is the comment in triple codes and this is multiline comments '''
-print("sushmita ray")
-print("is in btech")  # this line is automatically in next line
-print("ray","choudhary") # automatically space generate btw ray and choudhary
-print("ray" "choudhary") # not space generate btw ray and choudhary
-
